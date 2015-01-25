a0b35f25971003d6625f31771abe085ac6baef84
DDS63B <danilodiogo@gmail.com>
2015-01-25 10:44:09 +0100

    adjusting comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,20 +4,25 @@
 ## Write a short comment describing this function
 
 #The first function, creates a special "vector", which is really a list containing a function to
-#set the value of the vector
-#get the value of the vector
-#set the value of the mean
-#get the value of the mean
+
 makeCacheMatrix <- function(x = numeric()) {
         inv <- NULL
+
+#set the value of the vector
         set <- function(y) {
                 x <<- y
                 inv <<- NULL
         }
-        get <- function() x
-        setinv <- function(inverse) inv <<- inverse
-        getinv <- function()inv 
-        list(set = set, get = get,
+
+ #get the value of the vector
+       get <- function() x
+
+ #set the value of the inverse
+       setinv <- function(inverse) inv <<- inverse
+
+ #get the value of the inverse
+       getinv <- function()inv 
+       list(set = set, get = get,
              setinv = setinv,
              getinv = getinv)
 }
@@ -29,14 +34,17 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
+
+## Get inverse value from cache if it exists 
        inv <- x$getinv()
         if(!is.null(inv)) {
                 message("getting cached data")
                 return(inv)
         }
         data <- x$get()
+## If cache is blank calculate inverse 
         inv <- solve(data)
         x$setinv(inv)
+## Return a matrix that is the inverse of 'x'
        inv 
 }
-        ## Return a matrix that is the inverse of 'x'
